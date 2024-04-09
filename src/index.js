1eff683eefc9b157cc956982d957c6da866736ee
ssidhu271 <ssidhu271@my.bcit.ca>
2024-04-08 20:49:29 -0700

    Have floor map working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FloorMap from './FloorMap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FloorMap peopleCount={5}/>
   </React.StrictMode>
 );
 
