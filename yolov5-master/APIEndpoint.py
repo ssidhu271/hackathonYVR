baa87edfb7de07da5c8d0da8af788b9a55fdbb0f
Sungmok Cho <chosm7489@gmail.com>
2024-04-10 21:41:11 -0700

    set up multiple video streams and API endpoint to pass objects counts

--- yolov5-master/APIEndpoint.py
+++ yolov5-master/APIEndpoint.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify
-import detect
 
 # Create a Flask app instance
 app = Flask(__name__)
 
 # Define a route that will return the num_persons variable
-@app.route('/get_num_persons', methods=['GET'])
-def get_num_persons():
-    with open("num_persons.txt", "r") as f:
-        num_persons = f.read()
-    return jsonify({'num_persons': num_persons})
+@app.route('/get_counts', methods=['GET'])
+def get_counts():
+    counts = {}
+    with open("stream_counts.txt", "r") as f:
+        for line in f:
+            stream_id, objects_count_str = line.strip().split(': ', 1)
+            counts[stream_id] = eval(objects_count_str)
+    return jsonify(counts)
 
 
 # Run the Flask app
