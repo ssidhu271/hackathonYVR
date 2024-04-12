67091181b22a958ddac77a691f913fdc3cf0dbd2
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2024-04-12 08:05:01 -0700

    swapped position 2 markers

--- src/scripts/FloorMap.js
+++ src/scripts/FloorMap.js
@@ -123,30 +123,30 @@ const VancouverAirportMap = () => {
         ...prevMarkers,
         {
           id: fetchedData.camera1.person,
-          position: [49.1951, -123.1776],
+          position: [49.1948, -123.175],
           info: "Camera 1 from backend",
           peopleCount: fetchedData.camera1.person,
         },
         {
+
           id: fetchedData.camera2.person,
-          position: [49.194, -123.176],
+          position: [49.1946, -123.175],
           info: "Camera 2 from backend",
           peopleCount: fetchedData.camera2.person,
         },
       ]);
     }
   }, [fetchedData]);
-
   const [markers, setMarkers] = useState([
     {
       id: 1,
-      position: [49.1948, -123.175],
+      position: [49.1951, -123.1776],
       info: "Marker 1 Info",
       peopleCount: 20,
     },
     {
       id: 2,
-      position: [49.1946, -123.175],
+      position: [49.194, -123.176],
       info: "Marker 2 Info",
       peopleCount: 100,
     },
@@ -277,7 +277,7 @@ const VancouverAirportMap = () => {
   </div>
   <div className="predictedGraphContainer" style={{zIndex: '3'}}>
     <PredictedGraph />
-  </div><div className="barChartContainer" style={{zIndex: '502', width: 100 + '%'}}>
+  </div><div className="barChartContainer" style={{zIndex: '502'}}>
     <BarChart markers={markers} />
   </div>
   <div className="activityTypeContainer" style={{zIndex: '501'}}>
